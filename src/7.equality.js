7037a27cbd778c3445fcdb2498c091fda163b461
Xiao Xiao <49316508+Xiao0909@users.noreply.github.com>
2020-09-04 23:35:34 -0400

    finished test equality

--- src/7.equality.js
+++ src/7.equality.js
@@ -1,15 +1,27 @@
 // strictEquality: compare value & type
 // Example: strictEquality(1, true) => false
 // Example: strictEquality(1, 1) => true
-function strictEquality(val1, val2) {
-  return ;
+function strictEquality(val1, val2, result) {
+  if(val1 === val2){
+    result = true;
+  }
+  else{
+    result = false;
+  }
+  return result;
 }
 //equality: compare value but not necessarily the type
 // Example: equality(1, true) => true
 // Example: equality(1, 1) => true
 // Example: equality(1, false) => false
-function equality(val1, val2) {
-  return ;
+function equality(val1, val2, result) {
+  if(!(val1 == val2)){
+    result = false;
+  }
+  else{
+    result=true; 
+  }
+  return result;
 }
 
 module.exports = {
