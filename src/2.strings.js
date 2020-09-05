8a162983696fa2002554ba93984c9f9808771c5a
Xiao Xiao <49316508+Xiao0909@users.noreply.github.com>
2020-09-04 23:21:45 -0400

    finished test strings

--- src/2.strings.js
+++ src/2.strings.js
@@ -9,19 +9,22 @@ function reverseString(str) {
    * 
    * Can even be done on one line
    */
-  return ;
+  const result = str.split("").reverse().join("")
+  return result;
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+  const result = str.toUpperCase()
+  return result;
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+  const result = str.split(splitAt)
+  return result;
 }
 module.exports = {
   reverseString,
